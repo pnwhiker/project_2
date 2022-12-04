f858ede7ac6ab4e61bab3bdc834656e8e2fd9538
Ryan Whitney <whitney.ryan.a@gmail.com>
2022-12-04 13:15:00 -0800

    view test is working for root route!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 # require 'sinatra'
 
 class ApplicationController < Sinatra::Base
+
+    configure do
+        set :views, 'app/views'
+    end
+
     get '/' do
         erb :startup_page
     end
