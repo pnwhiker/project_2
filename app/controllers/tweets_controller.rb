c6fec471bc2ebc2b158c52f39d97004e77a4210e
Ryan Whitney <whitney.ryan.a@gmail.com>
2022-12-05 13:34:30 -0800

    adding controllers for each section

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
+    # get 'tweets/:id' do
+    #     params[:id]
+    # end
+
+    # returning all tweets currently in db
+    get '/tweets' do
+        erb :index
+    end
 
 
 end
